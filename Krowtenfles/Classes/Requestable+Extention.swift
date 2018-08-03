540e97fe1fc6bccd4d97c396a582584b1961002e
Vu Bach <bachndit@gmail.com>
2018-08-04 02:04:29 +0700

    fix setup lib

--- Krowtenfles/Classes/Requestable+Extention.swift
+++ Krowtenfles/Classes/Requestable+Extention.swift
@@ -1,57 +1,57 @@
-////
-////  Requestable+Extention.swift
-////  BBreviary
-////
-////  Created by BachVQ on 7/6/18.
-////  Copyright © 2018 BachVQ. All rights reserved.
-////
 //
-//import Foundation
-//import Alamofire
+//  Requestable+Extention.swift
+//  BBreviary
 //
-//extension Requestable {
-//    
-//    var method: HTTPMethod {
-//        get {
-//            return .get
-//        }
-//        set {}
-//    }
-//    
-//    var params: [String: String]? {
-//        get {
-//            return nil
-//        }
-//        set {}
-//    }
-//    
-//    // Config Header for Request
-//    var header: [String: String]? {
-//        get {
-//            return nil
-//        }
-//        set {}
-//    }
-//    
-//    func execute(dispatcher: NetworkSession = NetworkSession.shared, onSuccess: @escaping CompletionHandler<ResponseType>, onError: @escaping CompletionHandler<Error>) {
-//        let requestData = RequestConfiguration(path: path, method: method, params: params, headers: header)
-//        
-//        dispatcher.requestDispatch(request: requestData , onSuccess: { (responseData: Data) in
-//            do {
-//                let jsonDecoder = JSONDecoder()
-//                let result = try jsonDecoder.decode(ResponseType.self, from: responseData)
-//                DispatchQueue.main.async {
-//                    onSuccess(result)
-//                }
-//            } catch let error {
-//                DispatchQueue.main.async {
-//                    onError(error)
-//                }
-//            }
-//        }) { (error) in
-//            DispatchQueue.main.async {
-//                onError(error)
-//            }
-//        }
-//    }
-//}
+//  Created by BachVQ on 7/6/18.
+//  Copyright © 2018 BachVQ. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+extension Requestable {
+    
+    var method: HTTPMethod {
+        get {
+            return .get
+        }
+        set {}
+    }
+    
+    var params: [String: String]? {
+        get {
+            return nil
+        }
+        set {}
+    }
+    
+    // Config Header for Request
+    var header: [String: String]? {
+        get {
+            return nil
+        }
+        set {}
+    }
+    
+    func execute(dispatcher: NetworkSession = NetworkSession.shared, onSuccess: @escaping CompletionHandler<ResponseType>, onError: @escaping CompletionHandler<Error>) {
+        let requestData = RequestConfiguration(path: path, method: method, params: params, headers: header)
+        
+        dispatcher.requestDispatch(request: requestData , onSuccess: { (responseData: Data) in
+            do {
+                let jsonDecoder = JSONDecoder()
+                let result = try jsonDecoder.decode(ResponseType.self, from: responseData)
+                DispatchQueue.main.async {
+                    onSuccess(result)
+                }
+            } catch let error {
+                DispatchQueue.main.async {
+                    onError(error)
+                }
+            }
+        }) { (error) in
+            DispatchQueue.main.async {
+                onError(error)
+            }
+        }
+    }
+}
