8cf1949b77367861c396413c1305cdfc73681a33
Vu Bach <bachndit@gmail.com>
2018-08-04 02:04:29 +0700

    fix setup lib

--- Krowtenfles/Classes/Requestable.swift
+++ Krowtenfles/Classes/Requestable.swift
@@ -1,43 +1,43 @@
-////
-////  RequestType.swift
-////  BBreviary
-////
-////  Created by BachVQ on 7/5/18.
-////  Copyright © 2018 BachVQ. All rights reserved.
-////
 //
-//import Foundation
-////import Alamofire
+//  RequestType.swift
+//  BBreviary
 //
-//protocol Requestable {
-//    associatedtype ResponseType: Codable
-//    var path: String {get}
-//}
+//  Created by BachVQ on 7/5/18.
+//  Copyright © 2018 BachVQ. All rights reserved.
 //
-//protocol PostRequestable: Requestable {}
-//extension PostRequestable {
-//    var method: HTTPMethod {
-//        return .post
-//    }
-//}
-//
-//protocol GetRequestable: Requestable {}
-//extension GetRequestable {
-//    var method: HTTPMethod {
-//        return .get
-//    }
-//}
-//
-//protocol PutRequestable: Requestable {}
-//extension PutRequestable {
-//    var method: HTTPMethod {
-//        return .get
-//    }
-//}
-//
-//protocol DeleteRequestable: Requestable {}
-//extension DeleteRequestable {
-//    var method: HTTPMethod {
-//        return .delete
-//    }
-//}
+
+import Foundation
+import Alamofire
+
+protocol Requestable {
+    associatedtype ResponseType: Codable
+    var path: String {get}
+}
+
+protocol PostRequestable: Requestable {}
+extension PostRequestable {
+    var method: HTTPMethod {
+        return .post
+    }
+}
+
+protocol GetRequestable: Requestable {}
+extension GetRequestable {
+    var method: HTTPMethod {
+        return .get
+    }
+}
+
+protocol PutRequestable: Requestable {}
+extension PutRequestable {
+    var method: HTTPMethod {
+        return .get
+    }
+}
+
+protocol DeleteRequestable: Requestable {}
+extension DeleteRequestable {
+    var method: HTTPMethod {
+        return .delete
+    }
+}
